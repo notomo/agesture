f776ddeef22a19d03ab50d67284a215fdf2e50e1
notomo <notomo.motono@gmail.com>
2025-05-24 20:46:16 +0900

    Add option page link

--- src/entrypoints/popup/main.tsx
+++ src/entrypoints/popup/main.tsx
@@ -13,8 +13,17 @@ createRoot(container).render(
 
 function App() {
   return (
-    <div>
-      <p>This is React.</p>
+    <div className="px-6 py-4 border border-gray-300 bg-gray-50 rounded text-center">
+      <h1 className="text-2xl font-bold">agesture</h1>
+      <button
+        type="button"
+        onClick={() => {
+          browser.runtime.openOptionsPage();
+        }}
+        className="text-xl p-2 underline text-blue-500 whitespace-nowrap"
+      >
+        ⚙ Setting
+      </button>
     </div>
   );
 }

05845e33c551d96046f58809b96053d5a03c9f8e
notomo <notomo.motono@gmail.com>
2025-06-12 08:42:15 +0900

    Adjust newtab color

--- src/entrypoints/newtab/app.tsx
+++ src/entrypoints/newtab/app.tsx
@@ -2,8 +2,8 @@ import { App as ContentApp } from "../content/app";
 
 export function App() {
   return (
-    <div className="min-h-screen bg-gray-700 flex items-center justify-center">
-      <h1 className="text-gray-300 text-6xl font-bold">agesture</h1>
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-gray-800 text-6xl font-bold">agesture</h1>
       <ContentApp />
     </div>
   );
